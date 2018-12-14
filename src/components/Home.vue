114b50aa5828a8225ee4e33ddffa9355e2bc44e1
guzhenping <598343194@qq.com>
2018-12-14 11:11:49 +0800

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.svg">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from './subPages/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/components/test.vue
@@ -0,0 +1,21 @@
+<template>
+	
+	<div id="components-demo">
+	  <button v-on:click="count++">You clicked me {{ count }} times.</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "components-demo",
+		data: function () {
+			return {
+			  count: 0
+			}
+		},		
+	}
+</script>
+
+<style>
+
+</style>
